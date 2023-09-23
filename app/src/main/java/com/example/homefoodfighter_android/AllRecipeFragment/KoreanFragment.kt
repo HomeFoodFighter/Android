eced85251825f684c224cfc3f21528ebe9122c1e
YU JEONG SEON <yjs000616@gmail.com>
2023-09-23 21:09:32 +0900

    pull 받기

--- app/src/main/java/com/example/homefoodfighter_android/AllRecipeFragment/KoreanFragment.kt
+++ app/src/main/java/com/example/homefoodfighter_android/AllRecipeFragment/KoreanFragment.kt
@@ -1,16 +1,13 @@
 package com.example.homefoodfighter_android.AllRecipeFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.homefoodfighter_android.Adapter.AllRecipeRecycleAdapter
-import com.example.homefoodfighter_android.Adapter.RealTimeAdapter
 import com.example.homefoodfighter_android.Data.AllRecipeData
-import com.example.homefoodfighter_android.Data.RealTimeData
-import com.example.homefoodfighter_android.R
 import com.example.homefoodfighter_android.databinding.FragmentKoreanBinding
 
 class KoreanFragment : Fragment() {
@@ -37,6 +34,8 @@ class KoreanFragment : Fragment() {
         adapter.datalist=mDatas //데이터 넣어줌
         viewBinding.recyclerView.adapter=adapter //리사이클러뷰에 어댑터 연결
         viewBinding.recyclerView.layoutManager= GridLayoutManager(requireContext(),2) //레이아웃 매니저 연결
+        // RecyclerView의 아이템 높이를 wrap_content로 유지
+
     }
 
     fun initializelist(){ //임의로 데이터 넣어서 만들어봄
